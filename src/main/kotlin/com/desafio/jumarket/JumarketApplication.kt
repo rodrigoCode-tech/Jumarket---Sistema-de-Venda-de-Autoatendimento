3789246f2105add51bbe0e5532d020a335120f7e
Rodrigo <rodrigo>
2023-07-28 10:05:29 -0300

    Criação do projeto

---
+++ src/main/kotlin/com/desafio/jumarket/JumarketApplication.kt
@@ -0,0 +1,11 @@
+package com.desafio.jumarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JumarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<JumarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/desafio/jumarket/JumarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.desafio.jumarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JumarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
